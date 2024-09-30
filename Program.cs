a222b291071ec036c19db2fc07e0d5627d524be8
RakanEngineer <rakan.engineer@gmail.com>
2024-09-30 23:10:41 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using static System.Console;
+
+namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] weekdays = new string[7];
+            weekdays[0] = "Monday";
+            weekdays[1] = "Tuesday";
+            weekdays[2] = "Wednesday";
+            weekdays[3] = "Thursday";
+            weekdays[4] = "Friday";
+            weekdays[5] = "Saturday";
+            weekdays[6] = "Sunday";
+            foreach (string weekday in weekdays)
+            {
+                WriteLine(weekday);
+            }
+           
+        }
+    }
+}
